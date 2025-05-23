a910d4d263791f06752d04267932370c5f2b499d
JEREMIAS Sherwin <12300478@q10411.iutv.univ-paris13.fr>
2025-05-23 10:41:14 +0200

    Un test JUnit pour chaque methode

--- CalculatriceTest.java
+++ CalculatriceTest.java
@@ -1,1 +1,29 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
+public class CalculatriceTest {
+
+    private Calculatrice calculator;
+
+    @BeforeEach
+    public void start() {
+        calculator = new Calculatrice();
+    }
+
+    @Test
+    public void testAdd() {
+        assertEquals(5.0f, calculator.add(2.0f, 3.0f));
+    }
+
+    @Test
+    public void testDiv() {
+        assertEquals(2.0f, calculator.div(4.0f, 2.0f));
+    }
+
+    @Test
+    public void testMult() {
+        assertEquals(6.0f, calculator.mult(2.0f, 3.0f));
+    }
+
+}
