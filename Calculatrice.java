60af0d0f8696cb6e824def3e756ace49cd02dfe4
Sherwin <sherwin.jeremias@edu.univ-paris13.fr>
2025-05-23 10:22:41 +0200

    Create Calculatrice.java

---
+++ Calculatrice.java
@@ -0,0 +1,18 @@
+public class Calculatrice {
+  String marque;
+
+  public float add(float operande1, float operande2){
+    return operande1 + operande2;
+  }
+  
+  public float div(float numerateur, float denominateur) {
+    if(denominateur == 0){
+    throw new ArithmeticException("Div par O");
+    }
+    return numerateur / denominateur;
+  }
+  
+  public float mult(float operande1, float operande2){
+    return operande1 * operande2;
+  }
+}
